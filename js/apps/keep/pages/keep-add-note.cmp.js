69addcf203fa6f6a79a4cc1826ffa21caa592d38
tal0311 <tal.amit0311@gmail.com>
2022-03-02 16:31:34 +0200

    add new note

---
+++ js/apps/keep/pages/keep-add-note.cmp.js
@@ -0,0 +1,18 @@
+export default {
+  props: [],
+  template: `
+         <section>
+          <input type="text" name="title" placeholder="title"/>
+          <input type="text" placeholder="add note here" />
+          <input  title="Pin note" type="checkbox" name="isPinned"/>
+         </section>
+
+        `,
+  components: {},
+  created() {},
+  data() {
+    return {}
+  },
+  methods: {},
+  computed: {},
+}
--- js/apps/keep/pages/keep-index.js
+++ js/apps/keep/pages/keep-index.js
@@ -1,7 +1,9 @@
+import { keepService } from '../../../services/keep-service.js'
 import keepTodoCmp from './cmps/keep-todo.cmp.js'
 import keepVideoCmp from './cmps/keep-video.cmp.js'
 import keepTxtCmp from './cmps/keep-txt.cmp.js'
 import keepImgCmp from './cmps/keep-img.cmp.js'
+import keepAddNoteCmp from './keep-add-note.cmp.js'
 
 export default {
   name: 'keep-index',
@@ -9,6 +11,16 @@ export default {
     <section class="keep-index app-main">
     <h1>keep app</h1>
     
+    <section>
+      <!-- add new note -->
+      <button value="img">image</button>
+      <button>text</button>
+      <button>video</button>
+      <button>todo</button>
+       <component class="note" :is="cmp.type" :info="cmp.info" @selVal="setAns" :key="cmp.id"></component>
+      
+    </section>
+
     <!-- list -->
     <section class="notes-container" v-for="cmp in notes">
 
@@ -25,49 +37,18 @@ export default {
     keepVideoCmp,
     keepTxtCmp,
     keepImgCmp,
+    keepAddNoteCmp,
+  },
+  created() {
+    keepService.query().then((notes) => {
+      this.notes = notes
+      console.log(this.notes)
+    })
   },
   data() {
     return {
-      notes: [
-        {
-          id: 'n101',
-          type: 'keepTodoCmp',
-          isPinned: true,
-          info: {
-            txt: 'Fullstack Me Baby!',
-          },
-        },
-        {
-          id: 'n101',
-          type: 'keepTxtCmp',
-          isPinned: true,
-          info: {
-            txt: 'Fullstack Me Baby!',
-          },
-        },
-        {
-          id: 'n102',
-          type: 'keepVideoCmp',
-          info: {
-            url: 'http://some-img/me',
-            title: 'Bobi and Me',
-          },
-          style: {
-            backgroundColor: '#00d',
-          },
-        },
-        {
-          id: 'n103',
-          type: 'keepImgCmp',
-          info: {
-            label: 'Get my stuff together',
-            todos: [
-              { txt: 'Driving liscence', doneAt: null },
-              { txt: 'Coding power', doneAt: 187111111 },
-            ],
-          },
-        },
-      ],
+      notes: null,
+      noteType: '',
     }
   },
   methods: {
@@ -76,5 +57,4 @@ export default {
     },
   },
   computed: {},
-  created() {},
 }
