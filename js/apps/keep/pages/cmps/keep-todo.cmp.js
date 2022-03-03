77ba410f8bd158b023fd7deee30e38ea060df968
tal0311 <tal.amit0311@gmail.com>
2022-03-03 16:48:54 +0200

    pineed all components

--- js/apps/keep/pages/cmps/keep-todo.cmp.js
+++ js/apps/keep/pages/cmps/keep-todo.cmp.js
@@ -1,10 +1,12 @@
 export default {
   name: 'keep-todo',
-  props: ['info', 'noteStyle', 'noteId'],
+  props: ['note'],
   template: `
-       <section :style="{backgroundColor:color}">
-         <h4>{{info.label}}</h4>
-         <label htmlFor="done" v-for="todo in info.todos">
+
+  
+       <section :style="{backgroundColor:color}" v-bind="$attrs">
+         <h4>{{note.info.label}}</h4>
+         <label htmlFor="done" v-for="todo in note.info.todos">
                  <p>{{todo.txt}}</p>
                 <input type="checkbox" name="done" id="" />
                 <p>done at: {{todo.doneAt}}</p>
@@ -23,21 +25,25 @@ export default {
   created() {},
   data() {
     return {
-      color: this.noteStyle.backgroundColor,
+      color: this.note.style.backgroundColor,
     }
   },
   methods: {
+    unPin() {
+      console.log(this.note.id)
+      this.$emit('pin', this.note.id)
+    },
     addColor() {
-      this.$emit('change-color', this.color, this.noteId)
+      this.$emit('change-color', this.color, this.note.id)
       console.log(this.color)
     },
     remove() {
-      console.log('remove', this.noteId)
-      this.$emit('remove-note', this.noteId)
+      console.log('remove', this.note.id)
+      this.$emit('remove-note', this.note.id)
     },
     duplicate() {
-      console.log('dup', this.noteId)
-      this.$emit('duplicate-note', this.noteId)
+      console.log('dup', this.note.id)
+      this.$emit('duplicate-note', this.note.id)
     },
   },
   computed: {},
